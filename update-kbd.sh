e16819c5f3540a4d93cf46ac7d617a4e027a957d
Kz <parumu4github@gmail.com>
2020-05-05 16:56:05 +0900

    add update script. update keymap for 7skb

--- update-kbd.sh
+++ update-kbd.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-make 7skb:default:avrdude &
+keyboard=/dev/ttyACM0
+keymap=7skb:default
+
+make $keymap:avrdude &
+pid=$!
 
 while :
 do
-  ls -1 /dev/ttyACM0 2>/dev/null
+  ls -1 $keyboard 2>/dev/null
   if [ $? == 0 ]; then
-    echo "Found keyboard"
     break 
   fi
   sleep 1
 done
-sudo chmod 777 /dev/ttyACM0
+sudo chmod 777 $keyboard
 
+wait $pid
